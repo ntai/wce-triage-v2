4c2fb1ae7781b311d661ce0e03aa8a2091604a2d
Naoyuki Tai <ntaigit@cleanwinner.com>
2023-03-25 17:23:51 -0400

    Update setup-triage.sh

--- setup-triage.sh
+++ setup-triage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo -H apt install -y python3-pip --no-install-recommends
 sudo -H pip3 install --no-cache-dir -i https://test.pypi.org/simple/ --no-deps wce_triage
-python3 -m wce_triage.setup.setup_triage
+python3 -m wce_triage.setup.setup_triage_system
