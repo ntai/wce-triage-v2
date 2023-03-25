0bea4a3cc7284c5151d5ddee1fdca0b052b59120
Naoyuki Tai <ntai@cleanwinner.com>
2023-03-25 17:11:14 -0400

    short cut

---
+++ setup-triage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo -H apt install -y python3-pip --no-install-recommends
+sudo -H pip3 install --no-cache-dir -i https://test.pypi.org/simple/ --no-deps wce_triage
+python3 -m wce_triage.setup.setup_triage
